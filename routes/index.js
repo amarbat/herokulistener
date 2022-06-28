a5269b94463919587da27626c4ba3a53683535c6
AB <amarbat@gmail.com>
2022-06-28 00:52:34 -0400

    second commit

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let body = JSON.parse(req.body);
-  console.log (body);
+  console.log (req);
+  res.send ("Success");
+});
+
+router.post('/', function(req, res, next) {
+  console.log (req);
   res.send ("Success");
 });
 
