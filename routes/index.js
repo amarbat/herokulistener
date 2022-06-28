2e0c68e8aeab3d07d569de9e481dc385139c075d
AB <amarbat@gmail.com>
2022-06-28 11:44:54 -0400

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log (req.body);
+  console.log ("==========================");
+  console.log (req.headers);
   res.send ("Success");
 });
 
 router.post('/', function(req, res, next) {
   console.log (req.body);
+  console.log ("==========================");
+  console.log (req.headers);
   res.send ("Success");
 });
 
